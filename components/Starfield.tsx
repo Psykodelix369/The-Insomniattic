903e4efcecb5cc01130600ee79ad96dde1a91fb6
Psykodelix369 <psykodelix369@gmail.com>
2026-02-20 05:40:45 -0800

    Add crisp + hazy layered starfield

--- components/Starfield.tsx
+++ components/Starfield.tsx
@@ -3,10 +3,10 @@ import styles from "./Starfield.module.css";
 export default function Starfield() {
   return (
     <div className={styles.wrap} aria-hidden="true">
-      {/* Crisp / slightly brighter */}
+      {/* Crisp / brighter */}
       <div className={`${styles.layer} ${styles.setA} ${styles.crisp}`} />
 
-      {/* Hazy / blurred aura */}
+      {/* Hazy atmospheric layers */}
       <div className={`${styles.layer} ${styles.setB} ${styles.hazy}`} />
       <div className={`${styles.layer} ${styles.setC} ${styles.hazy}`} />
     </div>

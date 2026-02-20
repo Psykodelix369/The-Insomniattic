9d9af68ca8a196a6805af693ff420ce6abab2771
Psykodelix369 <psykodelix369@gmail.com>
2026-02-20 05:28:17 -0800

    Add hazy blur star layers and brighten crisp layer

--- components/Starfield.tsx
+++ components/Starfield.tsx
@@ -3,9 +3,12 @@ import styles from "./Starfield.module.css";
 export default function Starfield() {
   return (
     <div className={styles.wrap} aria-hidden="true">
-      <div className={`${styles.layer} ${styles.setA}`} />
-      <div className={`${styles.layer} ${styles.setB}`} />
-      <div className={`${styles.layer} ${styles.setC}`} />
+      {/* Crisp / slightly brighter */}
+      <div className={`${styles.layer} ${styles.setA} ${styles.crisp}`} />
+
+      {/* Hazy / blurred aura */}
+      <div className={`${styles.layer} ${styles.setB} ${styles.hazy}`} />
+      <div className={`${styles.layer} ${styles.setC} ${styles.hazy}`} />
     </div>
   );
 }

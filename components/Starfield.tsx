1ef056db71b11f168f62c57ffd1ce09c745fe8d9
Psykodelix369 <psykodelix369@gmail.com>
2026-02-20 05:14:28 -0800

    Three alternating star layers

--- components/Starfield.tsx
+++ components/Starfield.tsx
@@ -3,8 +3,9 @@ import styles from "./Starfield.module.css";
 export default function Starfield() {
   return (
     <div className={styles.wrap} aria-hidden="true">
-      <div className={styles.layer} />
-      <div className={styles.layer2} />
+      <div className={`${styles.layer} ${styles.setA}`} />
+      <div className={`${styles.layer} ${styles.setB}`} />
+      <div className={`${styles.layer} ${styles.setC}`} />
     </div>
   );
 }

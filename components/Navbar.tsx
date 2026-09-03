9769690de8bfa13c96b01713adbe886094852e69
Psykodelix369 <psykodelix369@gmail.com>
2026-09-03 02:31:42 -0700

    Update Navbar.tsx
    new new

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,12 @@
-
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav style={{position:"fixed", top:0, width:"100%", padding:"1rem 2rem", background:"rgba(0,0,0,.4)"}}>
-      <Link href="/">The Insomniattic</Link>{" | "}
-      <Link href="/blog">Blog</Link>{" | "}
-      <Link href="/portfolio">Portfolio</Link>{" | "}
-      <Link href="/about">About</Link>
+    <nav className="navbar">
+      <Link href="/" className="navBrand">The Insomniattic</Link>
+      <Link href="/blog" className="navLink">Blog</Link>
+      <Link href="/portfolio" className="navLink">Portfolio</Link>
+      <Link href="/about" className="navLink">About</Link>
     </nav>
   );
 }
